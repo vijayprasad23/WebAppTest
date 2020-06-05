1a9e07674569b7fd79f14416a0628ea69d8f02d3
Vijay Prasad <psv@avcol.school.nz>
2020-06-05 14:41:53 +1200

    Add project files.

---
+++ WebAppTest/Data/WebAppTestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppTest.Models;
+
+namespace WebAppTest.Data
+{
+    public class WebAppTestContext : DbContext
+    {
+        public WebAppTestContext (DbContextOptions<WebAppTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppTest.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ WebAppTest/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebAppTest.Data;
+using WebAppTest.Models;
+
+namespace WebAppTest
+{
+    public class CreateModel : PageModel
+    {
+        private readonly WebAppTest.Data.WebAppTestContext _context;
+
+        public CreateModel(WebAppTest.Data.WebAppTestContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
